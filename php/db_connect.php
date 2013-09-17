74358640c7671cdfe428b326bd1427d8213a9673
gioch <georgechkhvirkia@gmail.com>
2013-09-17 11:06:46 +0300

    last update

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,6 +1,6 @@
 <?
  
-$Database = mysql_connect("localhost","username","login");
+$Database = mysql_connect("localhost","username","pass");
 mysql_select_db("database_name", $Database);
 
 mysql_query( 'SET NAMES utf8' ); 
